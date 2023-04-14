370913f1093b5d591e85022338d7944d7eb4b77b
abde fakallah <senyoupl@icloud.com>
2023-04-15 01:06:53 +0200

    dark + light mode system created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import Navbar from './Navbar'
 import Weather from './Weather'
 import Article from './Article'
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
 import './styles/App.scss'
 
-const ThemeContext = createContext(null);
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState({
+    body: 'isBodyDark',
+    rest: 'isDark'
+  })
+  const body = document.querySelector('body');
+
+  useEffect(() => {
+    const isLightMode = localStorage.getItem('mode');
+    if(isLightMode) {
+      body.classList.remove('isBodyDark')
+      body.classList.add('isBodyLight');
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('mode', theme.body)
+    body.classList.remove('isBodyDark', 'isBodyLight');
+    body.classList.add(theme.body);
+  }, [theme.body])
+
   return (
-    <ThemeContext.Provider>
+    <ThemeContext.Provider value={{theme, setTheme}}>
       <Navbar />
     </ThemeContext.Provider>
   )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,30 @@
+// La gestion de l'état du theme se fais intégralement sur le composant navbar
+// excepter l'initialisation qui elle se fais sur le composant App
+import { useContext, useMemo } from 'react';
 import './styles/Navbar.scss'
-import GitHubIcon from '@mui/icons-material/GitHub';
+import { ThemeContext } from './App'
+import { HiMoon } from 'react-icons/hi'
+import { AiFillGithub } from 'react-icons/ai'
+import { FiSun } from 'react-icons/fi'
 
 function Navbar() {
+    const {theme, setTheme} = useContext(ThemeContext)
+
+    const handleClick = () => {
+        setTheme(prevTheme => {
+            const newBodyTheme = prevTheme.body === 'isBodyDark' ? 'isBodyLight' : 'isBodyDark';
+            const newRestTheme = prevTheme.rest === 'isDark' ? 'isLight' : 'isDark';
+            return { body: newBodyTheme, rest: newRestTheme }
+    })
+    }
+
     return (
-        <nav>
-            <ul className='navbar'>
-                <li>Test</li>
-                <li>Test</li>
+        <nav className={`navbar ${theme.rest}`}>
+            <ul className='menu'>
+                <li><AiFillGithub /></li>
+                <li onClick={handleClick}>
+                    {theme.rest === "isDark" ? <HiMoon className='isDark' /> : <FiSun className='isLight' />}
+                </li>
             </ul>
         </nav>
     )
